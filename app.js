f98bb1ac310ea44820bc9117a0417da70846ccc7
Vansh Devgan <vanshdevgans@gmail.com>
2021-09-02 16:31:27 +0530

    added

--- app.js
+++ app.js
@@ -107,7 +107,7 @@ app.use(function (err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get("env") === "development" ? err : {};
 
-  // render the error page
+  // render the error page now
   res.status(err.status || 500);
   res.render("error");
 });
