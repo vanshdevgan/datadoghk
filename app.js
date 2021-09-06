d6043ffbc2c73830a733adc1ed51f70af8340af2
Vansh Devgan <vanshdevgans@gmail.com>
2021-09-06 14:55:19 +0530

    A

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 require("dotenv").config();
   const createError = require("http-errors");
 const express = require("express");
-const path = require("path");
+ const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const mongoose = require("mongoose");
